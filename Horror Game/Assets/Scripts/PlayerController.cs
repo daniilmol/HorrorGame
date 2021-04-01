8077823ae6184928da5c06f2bfe378b6a51d2c5c
Angyeoung <realandreyb@gmail.com>
2021-03-31 19:07:55 -0800

    Footsteps+FX

--- Horror Game/Assets/Scripts/PlayerController.cs
+++ Horror Game/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,36 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
- 
 public class PlayerController : MonoBehaviour {
+    AudioSource footstepPlayer;    
+    Vector3 lastPosition;
+    [SerializeField]AudioClip[]footsteps;
+
+    void Start(){
+        footstepPlayer = GetComponent<AudioSource>();
+    }
+
+    void playFootsteps(){
+        int index = Random.Range(0, 7);
+        if(!footstepPlayer.isPlaying){
+            footstepPlayer.clip = footsteps[index];
+            footstepPlayer.Play(); 
+        }
+    }
+    void stopFootsteps(){
+
+    }
+
     void Update(){
-        
+        Vector3 currentPosition = transform.position;
+        if(currentPosition == lastPosition){
+            //Not Moving
+            stopFootsteps();
+        }
+        else{
+            //Moving
+            playFootsteps();
+        }
+        lastPosition = currentPosition;
     }
 }
