ab25d136cc61831b1769a67e787e3201fa3f4424
Angyeoung <realandreyb@gmail.com>
2021-04-19 01:02:58 -0700

    DOOR GO BOOM BOOM
    boomboombboomboombboomboombboomboombboomboombboomboombboomboomb

--- Horror Game/Assets/Custom Assets/Scripts/DoorInteraction.cs
+++ Horror Game/Assets/Custom Assets/Scripts/DoorInteraction.cs
@@ -6,10 +6,20 @@ public class DoorInteraction : Interactable {
     bool open = false;
     public override void Interact(){
         print("TOGGLING DOOR");
-        if(!GetComponent<Animator>().GetBool("isopen"))
+        if(!GetComponent<Animator>().GetBool("isopen") && !isPlaying(GetComponent<Animator>(), "Close Door")){
             GetComponent<Animator>().SetBool("isopen", true);
-        else
+            GetComponent<AudioSource>().Play();
+        }
+        else if(GetComponent<Animator>().GetBool("isopen") && !isPlaying(GetComponent<Animator>(), "Open Door")){
             GetComponent<Animator>().SetBool("isopen", false);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    bool isPlaying(Animator anim, string stateName){
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) && anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            return true;
+        else
+            return false;
     }
     void Update(){
         checkAvailability(gameObject, 73);
